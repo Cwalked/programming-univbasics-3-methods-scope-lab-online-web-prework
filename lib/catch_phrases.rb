b6c6635c53b343bf7efdb5d90f464457f34887d9
Courtney Walker <cwalked@gmail.com>
2019-09-02 01:54:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,14 +4,14 @@ def mario
 end
 
 def toadstool
-  status = "Thank you Mario! But Our Princess Is In Anothe Castle!"
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 def link
-  phrase = "It's Dangerouse To Go Alone! Take This."
+  phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
-def any_phrase
+def any_phrase(phrase)
   phrase = "Do A Barrel Roll!"
   puts phrase
 end
